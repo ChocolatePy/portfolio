f644b05921f1c6cdb51c2cab80ae7736a1414b98
chico <fxchiang@gmail.com>
2016-04-16 13:02:15 -0300

    simple templates added for gallery

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import GalleryItem
+from .models import Gallery
+
 # Create your views here.
+def gallery(request):
+    items = GalleryItem.objects.filter(published=True)
+
+    context = {
+            'title': 'Fotos',
+            'items': items,
+    }
+
+    return render(request, 'gallery/gallery.html', context)
+
